98b362f59cd61a6efb62dcf63110f1e90918a190
SoThundyy <mattiaf764@gmail.com>
2023-03-01 22:21:46 +0100

    resource di prova magnolia-cms nextjs

---
+++ magnolia.config.js
@@ -0,0 +1,10 @@
+import Basic from "@/templates/pages/Basic";
+import Headline from '@/templates/components/Headline';
+
+export const config = {
+    componentMappings: {
+      "next-demo-lm:pages/basic": Basic,
+  
+      "next-demo-lm:components/headline": Headline,
+    }
+  }
---
+++ src/pages/cms/PadeLoader.js
@@ -0,0 +1,112 @@
+import React from "react";
+import config from "../../../magnolia.config";
+import {
+  getLanguages,
+  removeCurrentLanguage,
+  getCurrentLanguage,
+} from "./AppHelpers";
+
+import { EditablePage } from "@magnolia/react-editor";
+import { EditorContextHelper } from "@magnolia/react-editor";
+
+class PageLoader extends React.Component {
+  state = {};
+
+  getPagePath = () => {
+    const languages = getLanguages();
+    const nodeName = process.env.NEXT_PUBLIC_MGNL_APP_BASE;
+    const currentLanguage = getCurrentLanguage();
+    
+    console.log("pathname:" + window.location.pathname);
+    console.log("nodeName:" + nodeName);
+
+      let path =
+      nodeName +
+      window.location.pathname.replace(
+        new RegExp("(.*" + process.env.NEXT_PUBLIC_APP_BASE + "|.html)", "g"),
+        ""
+      );
+
+      console.log("path:" + path);
+
+    if (currentLanguage !== languages[0]) {
+      path = removeCurrentLanguage(path, currentLanguage);
+      path += "?lang=" + currentLanguage;
+    }
+
+    return path;
+  };
+
+  loadPage = async () => {
+    // Bail out if already loaded content.
+    if (this.state.pathname === window.location.pathname) return;
+
+    const apiBase = "localhost:8080";
+
+    //const pagePath = this.getPagePath();
+    const pagePath = this.props.pagePath ? this.props.pagePath : this.getPagePath()
+
+    
+    console.log("pagePath:" + pagePath);
+    let fullContentPath =
+      apiBase + process.env.NEXT_PUBLIC_MGNL_API_PAGES + pagePath;
+
+    const pageResponse = await fetch(fullContentPath);
+    const pageJson = await pageResponse.json();
+    console.log("page content: ", pageJson);
+
+    const templateId = pageJson["mgnl:template"];
+    console.log("templateId:", templateId);
+
+    debugger;
+
+    let templateJson = null;
+    if (EditorContextHelper.inEditor()) {
+      const templateResponse = await fetch(
+        apiBase + process.env.NEXT_PUBLIC_MGNL_API_TEMPLATES + "/" + templateId
+      );
+      templateJson = await templateResponse.json();
+      console.log("definition:", templateJson);
+    }
+
+    this.setState({
+      init: true,
+      content: pageJson,
+      templateDefinitions: templateJson,
+      pathname: window.location.pathname,
+    });
+  };
+
+  inEditorPreview() {
+    const url = window.location.href;
+    const inPreview = url.indexOf("mgnlPreview=true") > 0;
+    console.log("inEditorPreview:" + inPreview);
+    return EditorContextHelper.inEditor() && inPreview;
+  }
+
+  componentDidMount() {
+    this.loadPage();
+  }
+
+  componentDidUpdate() {
+    this.loadPage();
+  }
+
+  render() {
+    if (this.state.init) {
+      console.log("config:", config);
+      
+      return (
+        <EditablePage
+          templateDefinitions={this.state.templateDefinitions || {}}
+          content={this.state.content}
+          config={config}
+        ></EditablePage>
+      );
+    } else {
+      return <p>NO PAGE.</p>;
+    }
+  }
+}
+
+export default PageLoader;
